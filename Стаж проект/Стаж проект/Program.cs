e8b7634a6a6f6a993aa8c14336fa359d450ee5ad
SylphMich <magisakadzhiyan@gmail.com>
2022-08-03 11:28:58 +0300

    .

---
+++ Стаж проект/Стаж проект/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Стаж_проект
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
